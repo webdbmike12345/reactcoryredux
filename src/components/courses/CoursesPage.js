2d189ec805dd7aa81772bff3b01c57e83b0d58f2
webdbmike12345 <webdbmike@gmail.com>
2020-07-23 08:06:12 -0500

    10 and 11 and start of 12

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,60 +1,77 @@
 import React from "react";
 import { connect } from "react-redux";
-import * as courseActions from '../../redux/actions/courseActions';
-import PropTypes from 'prop-types';
-import {bindActionCreators} from 'redux';
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
+import { Redirect } from "react-router-dom";
+import Spinner from '../common/Spinner';
 
 class CoursesPage extends React.Component {
   state = {
-    course: {
-      title: "",
-    },
-  };
+    redirectToAddCoursePage: false
+  }
 
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course: course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
+  componentDidMount() {
+    const { courses, authors, actions } = this.props;
+    if (courses.length === 0) {
+      actions.loadCourses().catch((error) => {
+        alert("Loading courses failed " + error);
+      });
+    }
+    if (authors.length === 0) {
+      actions.loadAuthors().catch((error) => {
+        alert("Loading authors failed " + error);
+      });
+    }
+  }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
+      {this.state.redirectToAddCoursePage && <Redirect to="/course" /> }
         <h2>Courses</h2>
-        <h3>Add Courses</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
-        {this.props.courses.map(course => {
-          return <div key={course.title}>{course.title}</div>
-        })}
-      </form>
+        <Spinner />
+        <button 
+        style={{ marginBottom: 20}}
+        className="btn btn-primary add-course"
+        onClick={() => this.setState({ redirectToAddCoursePage: true})}
+        >Add Course</button>
+        <CourseList courses={this.props.courses} />
+      </>
     );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    courses: state.courses,
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map((course) => {
+            return {
+              ...course,
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
+            };
+          }),
+    authors: state.authors,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    actions: bindActionCreators(courseActions, dispatch)
-  }
-}
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+    },
+  };
+};
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
-}
+  actions: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(CoursesPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,9 +1,17 @@
-import * as types from '../actions/actionTypes';
+import * as types from "../actions/actionTypes";
+import initialState from "./initialState";
 
-const courseReducer = (state = [], action) => {
+const courseReducer = (state = initialState.courses, action) => {
   switch (action.type) {
-    case types.CREATE_COURSE:
+    case types.CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
+    case types.UPDATE_COURSE_SUCCESS:
+      return state.map((course) =>
+        course.id === action.course.id ? action.course : course
+      );
+
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
     default:
       return state;
   }
